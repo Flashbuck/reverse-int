e12998e5e0d041829716beb8e8d9f6f53dbd48ee
Artoym <44746890+Flashbuck@users.noreply.github.com>
2020-10-06 13:16:50 +0300

    feat: add script task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = -n;
+    }
+    let numString = n.toString().split('');
+    return Number(numString.reverse().join(''));  
 }
